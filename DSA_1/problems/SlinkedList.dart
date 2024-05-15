70ae5fbefab4452c135d75750363eaac260163cb
Faisal <faisalerakottil@gmail.com>
2024-05-15 19:58:08 +0530

    same

--- DSA_1/problems/SlinkedList.dart
+++ DSA_1/problems/SlinkedList.dart
@@ -1,36 +1,36 @@
-class Node {
-  int data;
-  Node? next; 
-  Node(this.data);
-}
-class SLinkedList {
-  Node? head;
-  Node? tail;
-  void addNode(int data) {
-    Node newNode = Node(data);
-    if (head == null) {
-      head = newNode;
-    } else {
-      tail!.next = newNode;
-    }
-    tail = newNode;
-    print("Node added");
-  }
-  void display() {
-    if (head == null) {
-      print("Empty");
-      return;}
-    Node? temp = head;
-    while (temp != null) {
-      print(temp.data);
-      temp = temp.next;}
-  } 
-}
-void main(List<String> args) {
-  SLinkedList list = SLinkedList();
-  list.display();
-  list.addNode(10);
-  list.addNode(20);
-  list.addNode(50);
-  list.display();
-}
+// class Node {
+//   int data;
+//   Node? next; 
+//   Node(this.data);
+// }
+// class SLinkedList {
+//   Node? head;
+//   Node? tail;
+//   void addNode(int data) {
+//     Node newNode = Node(data);
+//     if (head == null) {
+//       head = newNode;
+//     } else {
+//       tail!.next = newNode;
+//     }
+//     tail = newNode;
+//     print("Node added");
+//   }
+//   void display() {
+//     if (head == null) {
+//       print("Empty");
+//       return;}
+//     Node? temp = head;
+//     while (temp != null) {
+//       print(temp.data);
+//       temp = temp.next;}
+//   } 
+// }
+// void main(List<String> args) {
+//   SLinkedList list = SLinkedList();
+//   list.display();
+//   list.addNode(10);
+//   list.addNode(20);
+//   list.addNode(50);
+//   list.display();
+// }
