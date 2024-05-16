91422b718ac0484e82f69a0ffee242ea7f3e63cf
Faisal <faisalerakottil@gmail.com>
2024-05-16 16:42:52 +0530

    number count program

---
+++ DSA_1/practice/countOfEvenNums.dart
@@ -0,0 +1,20 @@
+countingEvenNumbers(List<int> nums, size) {
+  int evencount = 0;
+  int oddCount = 0;
+  for (int i = 0; i < size; i++) {
+
+    if (nums[i]%2==0) {
+      evencount++;
+    } else {
+      oddCount++;
+    }
+  }
+  print('Number of even elements = ${evencount}\n');
+  print("Number of odd elements = ${oddCount}");
+}
+
+void main() {
+  List<int> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  int size = nums.length;
+  countingEvenNumbers(nums, size);
+}
